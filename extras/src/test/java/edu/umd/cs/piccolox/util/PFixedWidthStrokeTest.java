bfe97837b713ffe97aeb4f76fede4931bcd32df5
Marcus Rohrmoser <mrohrmoser@acm.org>
2008-07-30 13:03:46 +0000

    issue#48 started a unit test

---
+++ extras/src/test/java/edu/umd/cs/piccolox/util/PFixedWidthStrokeTest.java
@@ -0,0 +1,16 @@
+package edu.umd.cs.piccolox.util;
+
+import java.awt.BasicStroke;
+
+import junit.framework.TestCase;
+
+public class PFixedWidthStrokeTest extends TestCase {
+	public void testContants() {
+		assertEquals(PFixedWidthStroke.CAP_BUTT, BasicStroke.CAP_BUTT);
+		assertEquals(PFixedWidthStroke.CAP_ROUND, BasicStroke.CAP_ROUND);
+		assertEquals(PFixedWidthStroke.CAP_SQUARE, BasicStroke.CAP_SQUARE);
+		assertEquals(PFixedWidthStroke.JOIN_BEVEL, BasicStroke.JOIN_BEVEL);
+		assertEquals(PFixedWidthStroke.JOIN_MITER, BasicStroke.JOIN_MITER);
+		assertEquals(PFixedWidthStroke.JOIN_ROUND, BasicStroke.JOIN_ROUND);
+	}
+}
