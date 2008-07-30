0fc0382d8edefd14a8e6cb6fedb315311a8fd611
Marcus Rohrmoser <mrohrmoser@acm.org>
2008-07-30 17:36:53 +0000

    issue#45 add a site navigation

--- mvn-site-deploy.sh
+++ mvn-site-deploy.sh
@@ -27,32 +27,45 @@ svn="svn"
 # create a fresh site
 #mvn clean install site
 
-# prepare the material to upload
+echo "Checkout $base/release-$version"
 $svn mkdir $msg $base/release-$version 2> /dev/null
-rm -rf $tmp
-$svn checkout $base/release-$version $tmp
+# todo: checkout only if not there yet.
+#rm -rf $tmp
+#$svn checkout $base/release-$version $tmp
+$svn revert --recursive $tmp
+$svn update $tmp
 
-cp -r target/site $tmp/piccolo2d
-cp -r core/target/site $tmp/piccolo2d-core
-cp -r extras/target/site $tmp/piccolo2d-extras
-cp -r examples/target/site $tmp/piccolo2d-examples
+echo "Copy generated site to $tmp"
+cp -r target/site/* $tmp/piccolo2d
+cp -r core/target/site/* $tmp/piccolo2d-core
+cp -r extras/target/site/* $tmp/piccolo2d-extras
+cp -r examples/target/site/* $tmp/piccolo2d-examples
 
-# issue the (local) svn commands (delete/add)
+echo "Issue local svn delete/add commands"
 cd $tmp
 find . -type f | xargs svn status --verbose 2> /dev/null | egrep -e "^\!" | cut --characters=9- | xargs --no-run-if-empty $svn delete
 find . -mindepth 1 -type d | xargs --no-run-if-empty svn status --verbose 2> /dev/null | egrep -e "^\?" | cut --characters=9- | xargs --no-run-if-empty $svn add
 find . -type f | xargs svn status --verbose 2> /dev/null | egrep -e "^\?" | cut --characters=9- | xargs --no-run-if-empty $svn add --non-recursive
 
-# finish
+echo "Set mime-type"
 find . -name "*.html" | xargs $svn propset svn:mime-type text/html
 find . -name "*.xml" | xargs $svn propset svn:mime-type text/xml
 find . -name "*.css" | xargs $svn propset svn:mime-type text/css
 find . -name "*.gif" | xargs $svn propset svn:mime-type image/gif
 find . -name "*.png" | xargs $svn propset svn:mime-type image/png
-$svn $msg commit .
-$svn update
-$svn status
+#$svn $msg commit .
+#$svn update
+#$svn status
 
 # go back where we came from
 cd $cwd
+
+echo "###########################################################"
+echo " the site is generated and ready to upload."
+echo " "
+echo " Review the changes:"
+echo " $ svn status $tmp"
+echo " "
+echo " Do the upload:"
+echo " $ svn commit $tmp"
 
