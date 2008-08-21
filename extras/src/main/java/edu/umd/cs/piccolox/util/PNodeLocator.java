2be23215cdcb6f9595598213fe260fde1e764031
Michael L Heuer <heuermh@acm.org>
2008-08-21 16:00:40 +0000

    Issue 59 ; updating license header and adding one where missing

--- extras/src/main/java/edu/umd/cs/piccolox/util/PNodeLocator.java
+++ extras/src/main/java/edu/umd/cs/piccolox/util/PNodeLocator.java
@@ -1,5 +1,6 @@
 /*
- * Copyright (c) 2002-@year@, University of Maryland
+ * Copyright (c) 2008, Piccolo2D project, http://piccolo2d.org
+ * Copyright (c) 2002-2008, University of Maryland
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without modification, are permitted provided
@@ -12,8 +13,9 @@
  * and the following disclaimer in the documentation and/or other materials provided with the
  * distribution.
  *
- * Neither the name of the University of Maryland nor the names of its contributors may be used to
- * endorse or promote products derived from this software without specific prior written permission.
+ * None of the name of the University of Maryland, the name of the Piccolo2D project, or the names of its
+ * contributors may be used to endorse or promote products derived from this software without specific
+ * prior written permission.
  *
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED
  * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
@@ -23,10 +25,8 @@
  * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR
  * TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
  * ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- *
- * Piccolo was written at the Human-Computer Interaction Laboratory www.cs.umd.edu/hcil by Jesse Grosjean
- * under the supervision of Ben Bederson. The Piccolo website is www.cs.umd.edu/hcil/piccolo.
- */package edu.umd.cs.piccolox.util;
+ */
+package edu.umd.cs.piccolox.util;
 
 import edu.umd.cs.piccolo.PNode;
 
