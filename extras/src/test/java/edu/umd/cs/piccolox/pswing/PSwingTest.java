6aa549688ce7814b69c9b8a136ecd355b1540fa8
Michael L Heuer <heuermh@acm.org>
2009-03-28 03:15:24 +0000

    Issue 79 : removing testReferences method which incorrectly depends on certian behaviour from the garbage collector

--- extras/src/test/java/edu/umd/cs/piccolox/pswing/PSwingTest.java
+++ extras/src/test/java/edu/umd/cs/piccolox/pswing/PSwingTest.java
@@ -50,16 +50,4 @@ public class PSwingTest extends TestCase {
         frame.getCanvas().getLayer().addChild(pSwing);
         frame.setVisible(true);
     }
-
-    public void testReferences() {
-        WeakReference pSwing = new WeakReference(new PSwing(new JButton("test")), new ReferenceQueue());
-        PFrame frame = new PFrame();
-        frame.getCanvas().getLayer().addChild((PNode) pSwing.get());
-        frame.setVisible(true);
-        frame.getCanvas().getLayer().removeAllChildren();
-        for (int i=0; i<20; i++) { // make sure garbage collection has happened
-            System.gc();
-        }
-        assertTrue("The PSwing node should be garbage collected after removal", pSwing.isEnqueued());
-    }
 }
