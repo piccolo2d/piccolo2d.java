dca9e9e16cca8ea5b7d689029f8c09f7c1f081ef
Allain Lalonde <allain.lalonde@gmail.com>
2009-07-15 21:17:12 +0000

    Some tests for PDragEventHandler

---
+++ core/src/test/java/edu/umd/cs/piccolo/event/PDragEventHandlerTest.java
@@ -0,0 +1,21 @@
+package edu.umd.cs.piccolo.event;
+
+import junit.framework.TestCase;
+
+public class PDragEventHandlerTest extends TestCase {
+    private PDragEventHandler handler;
+
+    public void setUp() {
+        handler = new PDragEventHandler();
+    }
+    
+    public void testMoveToFrontOnPressDefaultToFalse() {
+        assertFalse(handler.getMoveToFrontOnPress());
+    }
+    
+    public void testMoveToFrontOnPressPersists() {
+        handler.setMoveToFrontOnPress(true);
+        assertTrue(handler.getMoveToFrontOnPress());
+    }
+        
+}
