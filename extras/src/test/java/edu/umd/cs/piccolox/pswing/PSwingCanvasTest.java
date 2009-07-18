1f544c7e3bf8f3c5578e71eb1857365e7a7931c3
Allain Lalonde <allain.lalonde@gmail.com>
2009-07-18 00:39:55 +0000

    Changed test name since it was incorrectly referring to PCanvas

--- extras/src/test/java/edu/umd/cs/piccolox/pswing/PSwingCanvasTest.java
+++ extras/src/test/java/edu/umd/cs/piccolox/pswing/PSwingCanvasTest.java
@@ -2,29 +2,29 @@ package edu.umd.cs.piccolox.pswing;
 
 import javax.swing.JPanel;
 
-import edu.umd.cs.piccolo.PCanvas;
 import junit.framework.TestCase;
 
 public class PSwingCanvasTest extends TestCase {
-    protected int pCanvasFinalizerCount;
+    protected int finalizerCallCount;
 
-    public void testMemoryLeakWithPCanvas() throws InterruptedException {                  
-        JPanel panel = new JPanel();       
-        for (int i=0; i < 10; i++) {            
+    public void testMemoryLeak() throws InterruptedException {
+        JPanel panel = new JPanel();
+        for (int i = 0; i < 10; i++) {
             PSwingCanvas canvas = new PSwingCanvas() {
                 public void finalize() {
-                    pCanvasFinalizerCount ++;                    
+                    finalizerCallCount++;
                 }
             };
             panel.add(canvas);
-            panel.remove(canvas);   
+            panel.remove(canvas);
             canvas = null;
         }
-        
+        panel = null;
         System.gc();
         System.runFinalization();
-        
-        // Not sure why I need -1 here, but I do. If I create 10000 it'll always be 1 less
-        assertEquals(10-1, pCanvasFinalizerCount);                  
+
+        // Not sure why I need -1 here, but I do. If I create 10000 it'll always
+        // be 1 less
+        assertEquals(10 - 1, finalizerCallCount);
     }
 }
