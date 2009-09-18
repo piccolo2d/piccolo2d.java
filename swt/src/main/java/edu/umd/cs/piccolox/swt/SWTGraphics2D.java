f73194a91499bd25914c404e3f140449700801b1
Michael L Heuer <heuermh@acm.org>
2009-09-18 06:38:55 +0000

    Issue 125 ; fix for compilation error with more recent versions of SWT dependency on linux, thanks to akurtakov for the patch

--- swt/src/main/java/edu/umd/cs/piccolox/swt/SWTGraphics2D.java
+++ swt/src/main/java/edu/umd/cs/piccolox/swt/SWTGraphics2D.java
@@ -273,7 +273,7 @@ public class SWTGraphics2D extends Graphics2D {
                     style = style | SWT.ITALIC;
                 }
 
-                return new Font(fd[0].getName(), style, fd[0].height);
+                return new Font(fd[0].getName(), style, fd[0].getHeight());
             }
             return null;
         }
