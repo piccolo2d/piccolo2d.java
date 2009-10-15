ba763f5f50e84b6d912033aab05d916354fcb9f2
Michael L Heuer <heuermh@acm.org>
2009-10-15 01:29:35 +0000

    Issue 132 ; fixing package names and removing Temp class

--- swt-examples/src/main/java/edu/umd/cs/piccolox/swt/examples/SWTBasicExample.java
+++ swt-examples/src/main/java/edu/umd/cs/piccolox/swt/examples/SWTBasicExample.java
@@ -26,7 +26,7 @@
  * TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
  * ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
-package edu.umd.cs.piccolo.examples.swt;
+package edu.umd.cs.piccolox.swt.examples;
 
 import java.awt.Color;
 
