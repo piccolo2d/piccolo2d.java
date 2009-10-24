5d50ee409c7ca32e8063ff1d54ba6e435490c202
Marcus Rohrmoser <mrohrmoser@acm.org>
2009-10-24 15:29:31 +0000

    Testcase for issue#142, followup to r816

--- extras/src/test/java/edu/umd/cs/piccolox/nodes/P3DRectTest.java
+++ extras/src/test/java/edu/umd/cs/piccolox/nodes/P3DRectTest.java
@@ -29,18 +29,24 @@
 package edu.umd.cs.piccolox.nodes;
 
 import java.awt.Color;
+import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
 
 import junit.framework.TestCase;
+import edu.umd.cs.piccolo.util.PPaintContext;
 
 /**
  * Unit test for P3DRect.
  */
 public class P3DRectTest extends TestCase {
     public void testClone() {
-        P3DRect rect = new P3DRect(10, 10, 10, 10);
+        final P3DRect rect = new P3DRect(10, 10, 10, 10);
         rect.setPaint(Color.BLUE);
-        P3DRect cloned = (P3DRect) rect.clone();
+        final P3DRect cloned = (P3DRect) rect.clone();
         assertNotNull(cloned);
-        assertEquals(Color.BLUE, cloned.getPaint());       
+        assertEquals(Color.BLUE, cloned.getPaint());
+
+        final BufferedImage img = new BufferedImage(3, 2, BufferedImage.TYPE_INT_ARGB);
+        cloned.paint(new PPaintContext((Graphics2D) img.getGraphics()));
     }
 }
