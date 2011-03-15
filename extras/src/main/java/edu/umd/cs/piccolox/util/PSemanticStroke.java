92873542c8e47151c16376202f9bea594add54f4
Michael L Heuer <heuermh@acm.org>
2011-03-15 22:28:41 +0000

    Issue 205 ; updating copyright year in license header to 2011

--- extras/src/main/java/edu/umd/cs/piccolox/util/PSemanticStroke.java
+++ extras/src/main/java/edu/umd/cs/piccolox/util/PSemanticStroke.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2008-2010, Piccolo2D project, http://piccolo2d.org
+ * Copyright (c) 2008-2011, Piccolo2D project, http://piccolo2d.org
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without modification, are permitted provided
