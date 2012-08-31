767fac02fe991e775c2e238f1431ecd7206f12d5
Michael L Heuer <heuermh@acm.org>
2012-08-31 16:42:25 +0000

    Issue 186 ; moving issue-related examples to examples.issues package, commit 2 of 2

--- examples/src/main/java/org/piccolo2d/examples/issues/FrameCanvasSizeBugExample.java
+++ examples/src/main/java/org/piccolo2d/examples/issues/FrameCanvasSizeBugExample.java
@@ -26,7 +26,7 @@
  * TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
  * ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
-package org.piccolo2d.examples;
+package org.piccolo2d.examples.issues;
 
 import java.awt.Color;
 
