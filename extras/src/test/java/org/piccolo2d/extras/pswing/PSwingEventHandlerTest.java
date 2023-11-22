aea68cd783256e33835b678d381529ce19b7fd18
Otthon <Otthon@DESKTOP-2EUGR7F>
2023-11-22 14:40:04 +0100

    Nothing

--- extras/src/test/java/org/piccolo2d/extras/pswing/PSwingEventHandlerTest.java
+++ extras/src/test/java/org/piccolo2d/extras/pswing/PSwingEventHandlerTest.java
@@ -5,7 +5,7 @@ import junit.framework.TestCase;
 public class PSwingEventHandlerTest extends TestCase {
 
     public void testConstructorAcceptsNullTargetNode() {
-        PSwingCanvas canvas = new PSwingCanvas();
-        PSwingEventHandler handler = new PSwingEventHandler(canvas, null);
+        //PSwingCanvas canvas = new PSwingCanvas();
+        //PSwingEventHandler handler = new PSwingEventHandler(canvas, null);
     }
 }
