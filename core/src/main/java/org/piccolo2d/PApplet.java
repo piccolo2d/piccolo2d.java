2b3f1e802ae4041b197cd28c7e3de63549ca7000
Otthon <Otthon@DESKTOP-2EUGR7F>
2023-11-22 15:11:47 +0100

    Packages 'core' and 'extras' are merged.

--- core/src/main/java/org/piccolo2d/PApplet.java
+++ core/src/main/java/org/piccolo2d/PApplet.java
@@ -26,12 +26,10 @@
  * TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
  * ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
-package org.piccolo2d.extras;
+package org.piccolo2d;
 
 import javax.swing.JApplet;
 import javax.swing.SwingUtilities;
-
-import org.piccolo2d.PCanvas;
 
 
 /**
