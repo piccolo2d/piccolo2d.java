69dd1826ea6b26b66c6f4d589e8703961584a291
Otthon <Otthon@DESKTOP-2EUGR7F>
2023-11-22 13:47:12 +0100

    Unnecessary import

--- extras/src/test/java/org/piccolo2d/extras/pswing/PSwingEventHandlerTest.java
+++ extras/src/test/java/org/piccolo2d/extras/pswing/PSwingEventHandlerTest.java
@@ -1,8 +1,5 @@
 package org.piccolo2d.extras.pswing;
 
-import org.piccolo2d.extras.pswing.PSwingCanvas;
-import org.piccolo2d.extras.pswing.PSwingEventHandler;
-
 import junit.framework.TestCase;
 
 public class PSwingEventHandlerTest extends TestCase {
