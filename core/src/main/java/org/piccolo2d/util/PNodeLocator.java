8b5e25813ee4ec0874844e69f9583bd97c4f0deb
Otthon <Otthon@DESKTOP-2EUGR7F>
2023-11-22 15:46:11 +0100

    Packages 'core' and 'extras' are merged.

--- core/src/main/java/org/piccolo2d/util/PNodeLocator.java
+++ core/src/main/java/org/piccolo2d/util/PNodeLocator.java
@@ -26,7 +26,7 @@
  * TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
  * ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
-package org.piccolo2d.extras.util;
+package org.piccolo2d.util;
 
 import org.piccolo2d.PNode;
 
