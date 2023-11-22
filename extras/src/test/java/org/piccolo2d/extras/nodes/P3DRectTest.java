af55528a33e38da81fb14e08304be174d350bf55
Otthon <Otthon@DESKTOP-2EUGR7F>
2023-11-22 13:11:14 +0100

    Unnecessary import

--- extras/src/test/java/org/piccolo2d/extras/nodes/P3DRectTest.java
+++ extras/src/test/java/org/piccolo2d/extras/nodes/P3DRectTest.java
@@ -32,7 +32,6 @@ import java.awt.Color;
 import java.awt.Graphics2D;
 import java.awt.image.BufferedImage;
 
-import org.piccolo2d.extras.nodes.P3DRect;
 import org.piccolo2d.util.PPaintContext;
 
 import junit.framework.TestCase;
