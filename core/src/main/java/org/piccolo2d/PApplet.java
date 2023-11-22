24b28c5f0f8a12ffe0ea4b2b61b61d512c030f6c
Otthon <Otthon@DESKTOP-2EUGR7F>
2023-11-22 16:35:57 +0100

    Source code reformatted

--- core/src/main/java/org/piccolo2d/PApplet.java
+++ core/src/main/java/org/piccolo2d/PApplet.java
@@ -31,7 +31,6 @@ package org.piccolo2d;
 import javax.swing.JApplet;
 import javax.swing.SwingUtilities;
 
-
 /**
  * <b>PApplet</b> is meant to be subclassed by applications that just need a
  * PCanvas embedded in a web page.
@@ -40,71 +39,70 @@ import javax.swing.SwingUtilities;
  * @author Jesse Grosjean
  */
 public class PApplet extends JApplet {
-    /** Used to allow versioned binary streams for serializations. */
-    private static final long serialVersionUID = 1L;
+	/** Used to allow versioned binary streams for serializations. */
+	private static final long serialVersionUID = 1L;
 
-    /** Canvas being displayed by this applet. */
-    private PCanvas canvas;
+	/** Canvas being displayed by this applet. */
+	private PCanvas canvas;
 
-    /**
-     * Initializes the applet with a canvas and no background.
-     */
-    public void init() {
-        setBackground(null);
+	/**
+	 * Initializes the applet with a canvas and no background.
+	 */
+	public void init() {
+		setBackground(null);
 
-        canvas = createCanvas();
-        getContentPane().add(canvas);
-        validate();
-        canvas.requestFocus();
-        beforeInitialize();
+		canvas = createCanvas();
+		getContentPane().add(canvas);
+		validate();
+		canvas.requestFocus();
+		beforeInitialize();
 
-        // Manipulation of Piccolo's scene graph should be done from Swings
-        // event dispatch thread since Piccolo is not thread safe. This code
-        // calls initialize() from that thread once the PFrame is initialized,
-        // so you are safe to start working with Piccolo in the initialize()
-        // method.
-        SwingUtilities.invokeLater(new Runnable() {
-            public void run() {
-                PApplet.this.initialize();
-                repaint();
-            }
-        });
-    }
+		// Manipulation of Piccolo's scene graph should be done from Swings
+		// event dispatch thread since Piccolo is not thread safe. This code
+		// calls initialize() from that thread once the PFrame is initialized,
+		// so you are safe to start working with Piccolo in the initialize()
+		// method.
+		SwingUtilities.invokeLater(new Runnable() {
+			public void run() {
+				PApplet.this.initialize();
+				repaint();
+			}
+		});
+	}
 
-    /**
-     * Returns the canvas this PApplet is displaying.
-     * 
-     * @return canvas this applet is displaying
-     */
-    public PCanvas getCanvas() {
-        return canvas;
-    }
+	/**
+	 * Returns the canvas this PApplet is displaying.
+	 * 
+	 * @return canvas this applet is displaying
+	 */
+	public PCanvas getCanvas() {
+		return canvas;
+	}
 
-    /**
-     * Provides an extension point for subclasses so that they can control
-     * what's on the canvas by default.
-     * 
-     * @return a built canvas
-     */
-    public PCanvas createCanvas() {
-        return new PCanvas();
-    }
+	/**
+	 * Provides an extension point for subclasses so that they can control what's on
+	 * the canvas by default.
+	 * 
+	 * @return a built canvas
+	 */
+	public PCanvas createCanvas() {
+		return new PCanvas();
+	}
 
-    /**
-     * This method will be called before the initialize() method and will be
-     * called on the thread that is constructing this object.
-     */
-    public void beforeInitialize() {
-    }
+	/**
+	 * This method will be called before the initialize() method and will be called
+	 * on the thread that is constructing this object.
+	 */
+	public void beforeInitialize() {
+	}
 
-    /**
-     * Subclasses should override this method and add their Piccolo2d
-     * initialization code there. This method will be called on the swing event
-     * dispatch thread. Note that the constructors of PFrame subclasses may not
-     * be complete when this method is called. If you need to initailize some
-     * things in your class before this method is called place that code in
-     * beforeInitialize();
-     */
-    public void initialize() {
-    }
+	/**
+	 * Subclasses should override this method and add their Piccolo2d initialization
+	 * code there. This method will be called on the swing event dispatch thread.
+	 * Note that the constructors of PFrame subclasses may not be complete when this
+	 * method is called. If you need to initailize some things in your class before
+	 * this method is called place that code in beforeInitialize();
+	 */
+	public void initialize() {
+	}
 }
--- core/src/main/java/org/piccolo2d/PComponent.java
+++ core/src/main/java/org/piccolo2d/PComponent.java
@@ -32,7 +32,6 @@ import java.awt.Cursor;
 
 import org.piccolo2d.util.PBounds;
 
-
 /**
  * Interface that a component needs to implement if it wants to act as a Piccolo
  * canvas.
@@ -42,37 +41,36 @@ import org.piccolo2d.util.PBounds;
  */
 public interface PComponent {
 
-    /**
-     * Called to notify PComponent that given bounds need repainting.
-     * 
-     * @param bounds bounds needing repaint
-     */
-    void repaint(PBounds bounds);
+	/**
+	 * Called to notify PComponent that given bounds need repainting.
+	 * 
+	 * @param bounds bounds needing repaint
+	 */
+	void repaint(PBounds bounds);
 
-    /**
-     * Sends a repaint notification the repaint manager if PComponent is not
-     * already painting immediately.
-     */
-    void paintImmediately();
+	/**
+	 * Sends a repaint notification the repaint manager if PComponent is not already
+	 * painting immediately.
+	 */
+	void paintImmediately();
 
-    /**
-     * Pushes the given cursor onto the cursor stack and sets the current cursor
-     * to the one provided.
-     * 
-     * @param cursor The cursor to set as the current one and push
-     */
-    void pushCursor(Cursor cursor);
+	/**
+	 * Pushes the given cursor onto the cursor stack and sets the current cursor to
+	 * the one provided.
+	 * 
+	 * @param cursor The cursor to set as the current one and push
+	 */
+	void pushCursor(Cursor cursor);
 
-    /**
-     * Pops the topmost cursor from the stack and sets it as the current one.
-     */
-    void popCursor();
+	/**
+	 * Pops the topmost cursor from the stack and sets it as the current one.
+	 */
+	void popCursor();
 
-    /**
-     * Sets whether the component is currently being interacted with.
-     * 
-     * @param interacting whether the component is currently being interacted
-     *            with
-     */
-    void setInteracting(boolean interacting);
+	/**
+	 * Sets whether the component is currently being interacted with.
+	 * 
+	 * @param interacting whether the component is currently being interacted with
+	 */
+	void setInteracting(boolean interacting);
 }
