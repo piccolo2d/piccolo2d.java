38bfd5685d77bec3645959afe9e0977352784ef5
Otthon <Otthon@DESKTOP-2EUGR7F>
2023-11-22 13:34:20 +0100

    Unnecessary import removed

--- swt/src/test/java/org/piccolo2d/extras/swt/PSWTHandleTest.java
+++ swt/src/test/java/org/piccolo2d/extras/swt/PSWTHandleTest.java
@@ -30,7 +30,6 @@ package org.piccolo2d.extras.swt;
 
 import org.piccolo2d.PNode;
 import org.piccolo2d.event.PInputEventListener;
-import org.piccolo2d.extras.swt.PSWTHandle;
 import org.piccolo2d.extras.util.PBoundsLocator;
 import org.piccolo2d.extras.util.PLocator;
 
